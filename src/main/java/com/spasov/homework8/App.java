40bb89a74bc81ffa6b55c84db0b620390298b16d
”Ivan <ivan.spasov203@gmail.com>
2024-04-28 20:38:54 +0700

    Config refactoring: using AnnotationConfigApplicationContext without .xml

--- src/main/java/com/spasov/homework8/App.java
+++ src/main/java/com/spasov/homework8/App.java
@@ -1,13 +1,15 @@
 package com.spasov.homework8;
 
+import com.spasov.homework8.config.SpringConfig;
 import com.spasov.homework8.entity.EnDayOfWeek;
 import com.spasov.homework8.entity.ItDayOfWeek;
 import com.spasov.homework8.service.CurrentDayOfWeekService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         CurrentDayOfWeekService currentDayOfWeekService = context.getBean("currentDayOfWeekService", CurrentDayOfWeekService.class);
         CurrentDayOfWeekService currentDayOfWeekService2 = context.getBean("currentDayOfWeekService", CurrentDayOfWeekService.class);
